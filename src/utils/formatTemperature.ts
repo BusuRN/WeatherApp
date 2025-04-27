61875252f8611a2f3bf440bb196d4f9cff2cf8bd
Busuioc Andrei <andreibusuioc@Busuiocs-MacBook-Pro.local>
2025-04-27 23:17:15 +0300

    implement favourite feature

--- src/utils/formatTemperature.ts
+++ src/utils/formatTemperature.ts
@@ -1,6 +1,7 @@
-
-
-const formatTemperature = (temp: number): string => {
+const formatTemperature = (temp?: number): string => {
+    if (typeof temp !== 'number') {
+        return '--';
+    }
     return `${temp.toFixed(1)}°C`;
 };
 
